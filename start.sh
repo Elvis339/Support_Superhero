fb0cb33d1609fae2dc223705d2c3d8d936e2693f
Elvis339 <elvissabanovic3@gmail.com>
2020-02-26 23:56:12 +0100

    updated start.sh

--- start.sh
+++ start.sh
@@ -3,27 +3,20 @@
 BUILD="$PWD/client/build/"
 MODULES="$PWD/node_modules"
 
-if [ ! -d "$MODULES" ]; then
-    echo "Installing modules in ${MODULES} ⚙️⚙️⚙️"
-    npm install && cd $PWD/client && npm install && cd ..
-fi
-
-if [ ! -d "$BUILD" ]; then
-    echo "Building frontend in ${BUILD}... 👷‍♂️"
-    npm run build_frontend
-else
-    rm -rf $BUILD
-    echo "Updating frontend... 🚧"
-    npm run build_frontend
-fi
-
-echo "----- FRONTEND BUILT & MODULES INSTALLED -----"
-
 echo "Set NODE_ENV production or development"
 read -p 'SET ENV: ' environment
 
-if [ $environment == "production" ]
-then
+if [ "$environment" == "production" ]; then
+    if [ ! -d "$BUILD" ]; then
+      echo "Building frontend in ${BUILD}... 👷‍♂️"
+      npm run build_frontend
+    else
+      rm -rf $BUILD
+      echo "Updating frontend... 🚧"
+      npm run build_frontend
+    fi
+  echo "----- FRONTEND BUILT & MODULES INSTALLED -----"
+
   export PORT=3000
   echo "PORT is set to 3000 🚢"
 
